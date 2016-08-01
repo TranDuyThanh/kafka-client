6ccfee51f2f57819219f209959bfafb8cf1c1edc
Khoi Nguyen <minhkhoi@siliconstraits.com>
2016-08-01 14:57:21 +0700

    fix message missing

--- common.go
+++ common.go
@@ -6,8 +6,9 @@ import (
 	"reflect"
 	"sync"
 
-	"github.com/Shopify/sarama"
 	"gopkg.in/bsm/sarama-cluster.v2"
+
+	"github.com/Shopify/sarama"
 )
 
 func execFunction(msg string, funcs ...interface{}) (result []reflect.Value, err error) {
@@ -43,12 +44,12 @@ func consumerGroupCallback(clusterConsumer *cluster.Consumer, funcs ...interface
 		for msg := range clusterConsumer.Messages() {
 
 			wg.Add(1)
-			go func() {
+			go func(msg *sarama.ConsumerMessage) {
 				defer wg.Done()
 				fmt.Printf("consumed with callback: %s/%d/%d\t\t\t%s\n", msg.Topic, msg.Partition, msg.Offset, msg.Value)
 				execFunction(string(msg.Value), funcs...)
 				clusterConsumer.MarkOffset(msg, "")
-			}()
+			}(msg)
 
 		}
 
@@ -67,11 +68,11 @@ func callback(messages chan *sarama.ConsumerMessage, funcs ...interface{}) {
 
 			wg.Add(1)
 
-			go func() {
+			go func(msg *sarama.ConsumerMessage) {
 				defer wg.Done()
 				fmt.Printf("consumed with callback: %s\n", string(msg.Value))
 				execFunction(string(msg.Value), funcs...)
-			}()
+			}(msg)
 		}
 
 		wg.Wait()
